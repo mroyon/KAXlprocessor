2fdb658ca321154f45fa9d5c64a8cac3beccc0fc
kuwaitarmy\mahmud <mahmud@kuwaitarmy.gov.kw>
2021-08-24 14:02:03 +0300

    test

--- solutions/Program.cs
+++ solutions/Program.cs
@@ -21,7 +21,6 @@ namespace ConsoleApp1
             using (var workbookDestination = new XLWorkbook())
             {
                 var worksheet = workbookDestination.Worksheets.Add("NewModed");
-
                 //start loop here
                 worksheet.Cell("A1").Value = "Hello World!";
                 worksheet.Cell("A2").FormulaA1 = "=MID(A1, 7, 5)";
